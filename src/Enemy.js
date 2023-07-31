fad302b2d543d9961eb731ce238180d2341d3683
unknown <danielledzp@gmail.com>
2023-07-30 23:14:12 -0300

    First commit

---
+++ src/Enemy.js
@@ -0,0 +1,136 @@
+import MovingDirection from "./MovingDirection.js";
+
+export default class Enemy {
+  constructor(x, y, tileSize, velocity, tileMap) {
+    this.x = x;
+    this.y = y;
+    this.tileSize = tileSize;
+    this.velocity = velocity;
+    this.tileMap = tileMap;
+
+    this.#loadImages();
+
+    this.moveDirection = Math.floor(
+      Math.random() * Object.keys(MovingDirection).length
+    );
+    this.directionTimerDefault = this.#random(10, 25);
+    this.directionTimer = this.directionTimerDefault;
+
+    this.scaredAboutToExpireTimerDefault = 10;
+    this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
+  }
+
+  draw(ctx, pause, pacman) {
+    if (!pause) {
+      this.#move();
+      this.#changeDirection();
+    }
+    this.#setImage(ctx, pacman);
+    ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+  }
+
+  collideWith(pacman) {
+    const size = this.tileSize / 2;
+    if (
+      this.x < pacman.x + size &&
+      this.x + size > pacman.x &&
+      this.y < pacman.y + size &&
+      this.y + size > pacman.y
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  #setImage(ctx, pacman) {
+    if (pacman.powerDotActive) {
+      this.#setImageWhenPowerDotIsActive(pacman);
+    } else {
+      this.image = this.normalGhost;
+    }
+    ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+  }
+
+  #setImageWhenPowerDotIsActive(pacman) {
+    if (pacman.powerDotAboutToExpire) {
+      this.scaredAboutToExpireTimer--;
+      if (this.scaredAboutToExpireTimer === 0) {
+        this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
+        if (this.image === this.scaredGhost) {
+          this.image = this.scaredGhost2;
+        } else {
+          this.image = this.scaredGhost;
+        }
+      }
+    } else {
+      this.image = this.scaredGhost;
+    }
+  }
+
+  #move() {
+    if (
+      !this.tileMap.didCollideWithEnviroment(this.x, this.y, this.moveDirection)
+    ) {
+      switch (this.moveDirection) {
+        case MovingDirection.up:
+          this.y -= this.velocity;
+          break;
+        case MovingDirection.down:
+          this.y += this.velocity;
+          break;
+        case MovingDirection.left:
+          this.x -= this.velocity;
+          break;
+        case MovingDirection.right:
+          this.x += this.velocity;
+          break;
+      }
+    }
+  }
+
+  #changeDirection() {
+    this.directionTimer--;
+    let newMoveDirection = null;
+    if (this.directionTimer == 0) {
+      this.directionTimer = this.directionTimerDefault;
+      newMoveDirection = Math.floor(
+        Math.random() * Object.keys(MovingDirection).length
+      );
+    }
+
+    if (newMoveDirection != null && this.moveDirection != newMoveDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        if (
+          !this.tileMap.didCollideWithEnviroment(
+            this.x,
+            this.y,
+            newMoveDirection
+          )
+        ) {
+          this.moveDirection = newMoveDirection;
+        }
+      }
+    }
+  }
+
+  #random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  #loadImages() {
+    this.normalGhost = new Image();
+    this.normalGhost.src = "../images/ghost.png";
+
+    this.scaredGhost = new Image();
+    this.scaredGhost.src = "../images/scaredGhost.png";
+
+    this.scaredGhost2 = new Image();
+    this.scaredGhost2.src = "../images/scaredGhost2.png";
+
+    this.image = this.normalGhost;
+  }
+}
